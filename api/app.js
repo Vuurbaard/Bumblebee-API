9a6a9c1183b4cc08351ec033986c5e8ab610c044
Sander <sanderbrokelman@msn.com>
2017-08-05 22:45:51 +0200

    Added word prediction on newly submitted yt url's

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 const fs = require('fs');
+const request = require('request');
 
 mongoose.connect(config.database);
 
@@ -57,3 +58,37 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log('Server listening on port', port);
 })
+
+
+// TEST:
+// var options = {
+//     url: 'https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true',
+//     // gzip: true,
+//     headers: {
+//         'Content-Type': 'audio/mpeg',
+//         // 'Transfer-Encoding': 'chunked',
+//     },
+//     body: fs.readFileSync(__dirname + '\\youtube\\SRRw1ERj2Gc.mp3'),
+//     // encoding: null,
+// };
+
+// request.post(options, function (err, response, body) {
+//     if (err) { 
+//         return console.error('upload failed:', err); 
+//     }
+
+//     var body = JSON.parse(body.toString());
+
+//     for(var result of body.results) {
+//         console.log(result);
+//         for(var alternative of result.alternatives) {
+//             for(var timestamp of alternative.timestamps) {
+//                 console.log(timestamp);
+//             }
+//         }
+//     }
+//     // console.log(body.results[0].alternatives);
+
+
+
+// }).auth('ca4e920b-6e5f-46a1-866f-33f904b65e73', 'iJktKEPGrhLn', false);
