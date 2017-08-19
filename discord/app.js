21bcdd139e0da70bf94f636b01d4b66fe29ecdc6
Ivo de Bruijn <ik@ivodebruijn.nl>
2017-08-19 16:10:02 +0200

    Improve stability if API fails

--- discord/app.js
+++ discord/app.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const request = require('request');
+const fs = require('fs');
 
 let authToken = "";
 
@@ -15,13 +16,19 @@ client.on('ready', () => {
 
   console.log('Trying to authenticate with Bumblebee API...');
   request.post(options, function (error, response, body) {
-    if(body.success) {
-      console.log('Authenticated!');
-      this.authToken = body.token;
+    if(body){
+      if(body.success) {
+        console.log('Authenticated!');
+        this.authToken = body.token;
+      }
+      else {
+        console.log(body.error);
+      }
+    }else{
+      // Seems like the API is down?
+      console.error("Cannot connect to the API", options);
     }
-    else {
-      console.log(body.error);
-    }
+
   });
 
 });
@@ -48,24 +55,35 @@ client.on('message', message => {
       console.log(error);
       console.log(body);
 
-      let filepath = __dirname + '/../api' + body.file;
+      if(body){
+        let filepath = __dirname + '/../api' + body.file;
 
-      console.log('Playing file:', filepath);
-      
-      const dispatcher = connection.playFile(filepath, function(err, intent) {
-        console.log('err:', err);
-        console.log('intent:', intent);
-      });
-      // Fixes delays after starting different streams
-      dispatcher.on('start', function() {
-        connection.player.streamingData.pausedTime = 0;
-      });
-      dispatcher.on('error',function(reason){
-        console.log('Dispatcher error ', reason)
-      });  
-      dispatcher.on('debug',function(info){
-        console.log(info)
-      });      
+        // Check if the file exists
+        if(fs.existsSync(filepath)){
+
+          console.log('Playing file:', filepath);
+          
+          const dispatcher = connection.playFile(filepath, function(err, intent) {
+            console.log('err:', err);
+            console.log('intent:', intent);
+          });
+          // Fixes delays after starting different streams
+          dispatcher.on('start', function() {
+            connection.player.streamingData.pausedTime = 0;
+          });
+          dispatcher.on('error',function(reason){
+            console.log('Dispatcher error ', reason)
+          });  
+          dispatcher.on('debug',function(info){
+            console.log(info)
+          });
+
+        }else{
+          console.error("File didn't exist while trying to play " + filepath);
+        }
+      }else{
+        console.error("Something went wrong doing the API request");
+      }
     });
       
   }).catch(err => console.log(err));
@@ -75,5 +93,5 @@ client.on('debug', info => {
     console.log(info);
 });
 
-client.login('MzEyNjU5NjYxNzc2MDkzMTg1.DFs3hA.WG_qmBq9RkZ4SM4sqo_LLA4BR0k');
+client.login('MzQwNjE3NzIwNjk1NjE5NTg1.DF1I2w.hD6Yb04HsGSdYxmc_AONFAwDvLM');
 
