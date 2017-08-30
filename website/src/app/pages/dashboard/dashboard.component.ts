0af71d4dc971f484f997617bbd58712c87bf706e
Sander <sanderbrokelman@msn.com>
2017-08-30 21:42:46 +0200

    more styling improvements (fixed navbar, side menu links styling, flashmessages fixed)

--- website/src/app/pages/dashboard/dashboard.component.ts
+++ website/src/app/pages/dashboard/dashboard.component.ts
@@ -1,37 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AudioService } from '../../services/audio.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 declare var WaveSurfer: any;
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  text: string = "Please let this work";
-  wavesurfer: any;
+	text: string = "Please let this work";
+	wavesurfer: any;
 
-  constructor(private audioService: AudioService) { }
+	constructor(private audioService: AudioService, private flashMessagesService: FlashMessagesService) { }
 
-  ngOnInit() {
-    var me = this;
-    this.wavesurfer = WaveSurfer.create({
-      container: '#waveform-tts',
-      waveColor: '#2b3e50',
-      progressColor: 'white'
-    });
+	ngOnInit() {
+		var me = this;
+		this.wavesurfer = WaveSurfer.create({
+			container: '#waveform-tts',
+			waveColor: '#2b3e50',
+			progressColor: 'white'
+		});
 
-    this.wavesurfer.on('ready', function () {
-      me.wavesurfer.play();
-    });
-  }
+		this.wavesurfer.on('ready', function () {
+			me.wavesurfer.play();
+		});
 
-  tts(text) {
-    this.audioService.tts(this.text).subscribe(data => {
-      console.log('tts result:', data);
-      
-    });
-  }
+	}
+
+	tts(text) {
+		this.audioService.tts(this.text).subscribe(data => {
+			console.log('tts result:', data);
+
+		});
+	}
 }
