12841748ae880816ef8290072fd2168e3f824406
Sander <sanderbrokelman@msn.com>
2017-10-08 20:51:34 +0200

    Simplifedieriderded

--- api/test.js
+++ api/test.js
@@ -10,49 +10,42 @@ var words = [
 	{
 		text: "please",
 		fragments: [{ source: 1 }],
-		links: ["let"]
 	},
 	{
 		text: "let",
 		fragments: [{ source: 1 }, { source: 2 }],
-		links: ["me", "this"]
 	},
 	{
 		text: "this",
 		fragments: [{ source: 1 }],
-		links: ["work"]
 	},
 	{
 		text: "work",
 		fragments: [{ source: 1 }],
-		links: []
 	},
 	{
 		text: "dont",
 		fragments: [{ source: 2 }],
-		links: ["let"]
 	},
 	{
 		text: "me",
 		fragments: [{ source: 2 }],
-		links: ["down"]
 	},
 	{
 		text: "down",
 		fragments: [{ source: 2 }],
-		links: []
 	},
 ]
 
 function magic() {
 
-	var combinations = new Array();
-	combinations.push(words[0]); // please
-	combinations.push(words[1]); // let
-	combinations.push(words[5]); // me
-	combinations.push(words[6]); // down
+	var input = new Array();
+	input.push(words[0]); // please
+	input.push(words[1]); // let
+	input.push(words[5]); // me
+	input.push(words[6]); // down
 
-	for (var word of combinations) {
+	for (var word of input) {
 		console.log(word);
 
 
