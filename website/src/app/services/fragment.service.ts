635a0007bdfa58c29467060c04e8ed9ea8cb0f19
Sander <sanderbrokelman@msn.com>
2017-12-02 17:42:23 +0100

    Fixed scores on edges

--- website/src/app/services/fragment.service.ts
+++ website/src/app/services/fragment.service.ts
@@ -7,31 +7,31 @@ import { isDevMode } from '@angular/core';
 @Injectable()
 export class FragmentService {
 
-  private host: String;
+	private host: String;
 
-  constructor(private authService: AuthService, private http: Http) {
+	constructor(private authService: AuthService, private http: Http) {
 
-    if(isDevMode()) {
-      this.host = 'http://localhost:3000';
-    }
-    else {
-      this.host = 'http://bumblebee.mijnproject.nu:3000';
-    }
-  }
+		if (isDevMode()) {
+			this.host = 'http://localhost:3000';
+		}
+		else {
+			this.host = 'http://bumblebee.mijnproject.nu:3000';
+		}
+	}
 
-  all(){
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Authorization', this.authService.getToken());
+	all() {
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		headers.append('Authorization', this.authService.getToken());
 
-    return this.http.get(this.host + '/audio/fragments',{headers: headers}).map(res => res.json());
-  }
+		return this.http.get(this.host + '/audio/fragments', { headers: headers }).map(res => res.json());
+	}
 
-  get( id : string ){
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Authorization', this.authService.getToken());
-    return this.http.get(this.host + '/audio/fragments?id=' + id, {headers: headers} ).map(res => res.json());
-  }
+	get(id: string) {
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+		headers.append('Authorization', this.authService.getToken());
+		return this.http.get(this.host + '/audio/fragments?id=' + id, { headers: headers }).map(res => res.json());
+	}
 
 }
