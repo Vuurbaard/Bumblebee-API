8b652110bb2ea244b6c6c98774213763b8194ea2
Sander <sanderbrokelman@msn.com>
2018-01-21 12:14:38 +0100

    API returns the words that it could not find now and some minor refactoring

--- api/routes/audio.js
+++ api/routes/audio.js
@@ -17,32 +17,32 @@ const Audio = require('../services/audio');
 const Fragments = require('../services/fragments');
 const Engine = require('../services/engine');
 
-router.post('/download', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.post('/download', passport.authenticate('jwt', { session: false }), (req, res, next) => {
 
 	let url = req.body.url;
-	
+
 	Audio.download(url).then(result => {
 		Audio.saveSource(result.file.id, result.origin).then(source => {
 
-			Fragments.getFragmentsBySource(source).then(function(fragments) {
+			Fragments.getFragmentsBySource(source).then(function (fragments) {
 
 				res.json({
-					url: result.file.publicpath, 
+					url: result.file.publicpath,
 					sourceId: source._id,
 					fragments: fragments
 				});
 			});
-			
+
 		});
 	});
 
 });
 
-router.post('/fragments', passport.authenticate('jwt', {session: false}), (req, res, next) => {
-    
+router.post('/fragments', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+
 	let sourceId = req.body.sourceId;
 	let fragments = req.body.fragments;
-	
+
 	Fragments.submit(sourceId, fragments).then(() => {
 		res.json({ success: true });
 	}).catch(err => {
@@ -51,7 +51,7 @@ router.post('/fragments', passport.authenticate('jwt', {session: false}), (req, 
 
 });
 
-router.get('/fragments', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+router.get('/fragments', passport.authenticate('jwt', { session: false }), (req, res, next) => {
 	res.json({ success: false });
 });
 
@@ -59,26 +59,28 @@ router.post('/tts', (req, res, next) => {
 
 	let text = req.body.text.toLowerCase();
 
-	Engine.asyncmagic(text).then(result => {
+	Engine.asyncmagic(text).then((result) => {
+
 		console.log('blackmagic result:', result);
 		res.json(result);
+
 	}).catch(error => {
 
-		if(error.stack) {
+		if (error.stack) {
 			console.log(error.stack.toString().red);
 		}
 		else {
 			console.log(error.toString().red);
 		}
 
-		if(error.status) {
+		if (error.status) {
 			res.status(error.status).json(error);
 		}
 		else {
 			res.status(500);
 		}
 	});
-    
+
 });
 
 
