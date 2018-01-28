e8d5473698d46b2f520ce5ddfee1faa2e973ea69
Sander <sanderbrokelman@msn.com>
2018-01-28 20:15:59 +0100

    Register page?

--- website2/src/app/pages/users/register/register.component.ts
+++ website2/src/app/pages/users/register/register.component.ts
@@ -1,15 +1,52 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../../services/authentication.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	name: String;
+	username: String;
+	email: String;
+	password: String;
 
-  ngOnInit() {
-  }
+	constructor(private authenticationService: AuthenticationService, private router: Router) { }
+
+	ngOnInit() {
+	}
+
+	onRegisterSubmit() {
+		const user = {
+			name: this.name,
+			email: this.email,
+			username: this.username,
+			password: this.password
+		}
+
+		// Check required fields
+		// if (!this.validateService.validateRegister(user)) {
+		// 	this.flashMessagesService.show('Please fill in all fields', { cssClass: 'alert-danger' });
+		// }
+
+		// // Validate email
+		// else if (!this.validateService.validateEmail(user.email)) {
+		// 	this.flashMessagesService.show('Please enter a valid email', { cssClass: 'alert-danger' });
+		// }
+
+		this.authenticationService.registerUser(user).subscribe(data => {
+			if (data.success) {
+				//this.flashMessagesService.show('You are now register and can login', { cssClass: 'alert-success' });
+				this.router.navigate(['/login']);
+			}
+			else {
+				//this.flashMessagesService.show('Something went wrong', { cssClass: 'alert-danger' });
+				this.router.navigate(['/register']);
+			}
+		});
+	}
 
 }
