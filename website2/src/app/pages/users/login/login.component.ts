799b568048a26005d30d85e9050fdd6f073f4875
Sander <sanderbrokelman@msn.com>
2018-01-28 19:44:56 +0100

    Login shizzle

--- website2/src/app/pages/users/login/login.component.ts
+++ website2/src/app/pages/users/login/login.component.ts
@@ -1,15 +1,44 @@
+import { AuthenticationService } from './../../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	username: string;
+	password: string;
 
-  ngOnInit() {
-  }
+	constructor(private authService: AuthenticationService, private router: Router) { }
 
+	ngOnInit() {
+	}
+
+	onLoginSubmit() {
+		const user = {
+			username: this.username,
+			password: this.password
+		}
+
+		this.authService.authenticateUser(user).subscribe(data => {
+			if (data.success) {
+				this.authService.storeUserData(data.token, data.user);
+				// this.flashMessagesService.show('You are now logged in', {
+				// 	cssClass: 'alert-success',
+				// 	timeout: 5000
+				// });
+				this.router.navigate(['dashboard']);
+			}
+			else {
+				// this.flashMessagesService.show(data.msg, {
+				// 	cssClass: 'alert-danger',
+				// 	timeout: 5000
+				// });
+				this.router.navigate(['login']);
+			}
+		});
+	}
 }
--- website2/src/app/services/authentication.service.ts
+++ website2/src/app/services/authentication.service.ts
@@ -7,8 +7,7 @@ import { environment } from '../../environments/environment';
 @Injectable()
 export class AuthenticationService {
 
-	constructor(private jwtHelper: JwtHelperService) {
-
+	constructor(private http: Http, private jwtHelper: JwtHelperService) {
 	}
 
 	login() {
@@ -16,12 +15,33 @@ export class AuthenticationService {
 	}
 
 	logout() {
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('user');
+	}
 
+	authenticateUser(user) {
+		let headers = new Headers();
+		headers.append('Content-Type', 'application/json');
+	
+		return this.http.post(environment.apiUrl + 'users/authenticate', user, {headers: headers}).map(res => res.json());
+	}
+
+	storeUserData(token, user) {
+		localStorage.setItem('access_token', token);
+		localStorage.setItem('user', JSON.stringify(user));
 	}
 
 	isLoggedIn() {
 		const token: string = localStorage.getItem('access_token');
 
     	return token != null && !this.jwtHelper.isTokenExpired(token);
+	}
+
+	get user() {
+		return JSON.parse(localStorage.getItem('user'));
+	}
+
+	get token() {
+		return localStorage.getItem('access_token');
 	}
 }
