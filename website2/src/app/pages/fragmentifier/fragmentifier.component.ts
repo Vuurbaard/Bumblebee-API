e323f1e58f0e8afd9586eb323911f904f40c8f6a
Sander <sanderbrokelman@msn.com>
2018-01-28 19:27:41 +0100

    Added empty needed pages

---
+++ website2/src/app/pages/fragmentifier/fragmentifier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fragmentifier',
+  templateUrl: './fragmentifier.component.html',
+  styleUrls: ['./fragmentifier.component.scss']
+})
+export class FragmentifierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
