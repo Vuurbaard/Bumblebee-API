06a386bee99b3a14e0dd085b3f546777a8f9d442
Sander <sanderbrokelman@msn.com>
2018-01-28 19:04:51 +0100

    More stuff

--- website2/src/app/app-routing.module.ts
+++ website2/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { RegisterComponent } from './pages/users/register/register.component';
+import { LoginComponent } from './pages/users/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'login', component: LoginComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
