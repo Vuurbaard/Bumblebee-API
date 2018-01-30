15f872383cdf3fd6d489fbf603472e51af35a774
Sander <sanderbrokelman@msn.com>
2018-01-30 23:53:26 +0100

    default redirect to /dashboard for now

--- website2/src/app/app-routing.module.ts
+++ website2/src/app/app-routing.module.ts
@@ -10,10 +10,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-	// { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'register', component: RegisterComponent },
 	{ path: 'login', component: LoginComponent },
-	{ path: 'dashboard', component: DashboardComponent },
 	{ path: 'tts', component: TtsComponent },
 	{ path: 'tts/:text', component: TtsComponent },
 	{ path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
