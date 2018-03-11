e927f6e872118fc169b6ecd9d298192cd7a2875e
Sander <sanderbrokelman@msn.com>
2018-03-11 19:09:35 +0100

    Automagically create discord users

--- discord/app.js
+++ discord/app.js
@@ -8,6 +8,7 @@ const client = new Discord.Client();
 const request = require('request');
 const fs = require('fs');
 const Queuer = require('./queuer');
+const randomstring = require('randomstring');
 
 let authToken = "";
 let queues = [];
@@ -15,6 +16,8 @@ let queues = [];
 client.on('ready', () => {
 	// Try to login
 	login();
+	client.user.setPresence({ game: { name: 'www.bumblebee.fm', type: 2 } }).then(console.log).catch(console.error);
+
 });
 
 client.on('message', message => {
@@ -25,6 +28,8 @@ client.on('message', message => {
 	if (!message.member) { return; }
 	if (!message.member.voiceChannel) { return; }
 	if (message.member.user.bot) { return; }
+
+	createOrUpdateUser(message);
 
 	// Generate unique name for the queue we are going to use
 	// Readable + unique
@@ -52,9 +57,8 @@ client.on('message', message => {
 					// let filepath = __dirname + '/../api' + body.file;
 					let filepath = 'http://' + api() + body.file;
 
-					console.log('Playing file:', filepath);					
+					console.log('Playing file:', filepath);
 
-					
 					const dispatcher = connection.playStream(filepath, function (err, intent) {
 						console.log('err:', err);
 						console.log('intent:', intent);
@@ -65,20 +69,20 @@ client.on('message', message => {
 					});
 
 					dispatcher.on('end', function () {
-					
+
 						var options = {
 							url: filepath,
 							headers: { 'Authorization': this.authToken }
 						};
-						request.delete(options, function(error, response, body) {
-							if(error || response && response.statusCode != 200) {
+						request.delete(options, function (error, response, body) {
+							if (error || response && response.statusCode != 200) {
 								console.log(error, response.statusCode, body);
 								console.log('failed to delete', options.url);
 							}
 							else {
 								console.log('deleted audio at', options.url);
 							}
-							
+
 						});
 
 						queuer.finish();
@@ -93,12 +97,12 @@ client.on('message', message => {
 						console.log(info)
 					});
 
-					if(body.wordsNotFound && body.wordsNotFound.length > 0) {
+					if (body.wordsNotFound && body.wordsNotFound.length > 0) {
 						message.reply('Missing words: ' + body.wordsNotFound);
 					}
 
 				}
-				else if(body.wordsNotFound && body.wordsNotFound.length > 0) {
+				else if (body.wordsNotFound && body.wordsNotFound.length > 0) {
 					message.reply('Missing words: ' + body.wordsNotFound);
 					queuer.finish();
 				}
@@ -128,7 +132,7 @@ function api() {
 	return process.env.API_HOST + ':' + process.env.API_PORT;
 }
 
-function token(){
+function token() {
 	return process.env.API_TOKEN;
 }
 
@@ -137,7 +141,7 @@ function login() {
 
 	var options = {
 		url: 'http://' + api() + '/login',
-		body: { "username": "Zunz", "password": "123" },
+		body: { "username": "Bumblebee", "password": "123" },
 		json: true
 	};
 
@@ -188,3 +192,42 @@ process.on('SIGINT', shutdown.bind());
 // // catches "kill pid" (for example: nodemon restart)
 process.on('SIGUSR1', shutdown.bind());
 process.once('SIGUSR2', shutdown.bind());
+
+function createOrUpdateUser(message) {
+	let username = message.member.user.username + "#" + message.member.user.discriminator;
+	let password = randomstring.generate({
+		length: 12,
+		charset: 'alphabetic'
+	});
+
+	var options = {
+		url: 'http://' + api() + '/users',
+		body: {
+			"externalId": message.member.user.id,
+			"name": message.member.user.username,
+			"username": username,
+			"password": password,
+			"avatar": message.member.user.avatarURL,
+		},
+		json: true,
+		headers: { 'Authorization': this.authToken }
+	};
+
+	request.post(options, function (error, response, body) {
+		if (body && body.success && body.created) {
+			console.log('Created or updated user', username);
+
+			let embed = new Discord.RichEmbed();
+			embed.setDescription("For your convinience I've created an account for you so you can add your own audio to my database.");
+			embed.setAuthor("Bumblebee", "https://www.dropbox.com/s/jl9h68lfk92j3q4/bumblee%20icon.png?dl=1", "https://bumblebee.fm");
+			embed.setTitle("https://bumblebee.fm");
+			embed.setURL("https://bumblebee.fm/login");
+			embed.setFooter('Navigate to the URL above to get started');
+			embed.setColor("#f6a821");
+		
+			embed.fields.push({ name: 'username', value: username });
+			embed.fields.push({ name: 'password', value: password });
+			message.member.send(embed);
+		}
+	});
+}
