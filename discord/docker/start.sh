9435f289a7e4c10064d742900db7ce9e8ca5c9b1
Ivo de Bruijn <ivo@idobits.nl>
2018-03-29 22:20:04 +0200

    Don't use nodemon on live

--- discord/docker/start.sh
+++ discord/docker/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 npm install
+
+USE_NODEMON=${USE_NODEMON:=true}
+
+if [ $USE_NODEMON = "true" ]; then \
+echo "Running via Nodemon";
 nodemon -L /var/www/html/app.js
+else
+echo "Running via Node";
+node /var/www/html/app.js
+fi
