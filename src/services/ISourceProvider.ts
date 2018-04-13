0140dd14e7ce3c24eea612de5074c3d762840303
Sander <sanderbrokelman@msn.com>
2018-04-13 22:49:04 +0200

    small refactoring of code

--- src/services/ISourceProvider.ts
+++ src/services/ISourceProvider.ts
@@ -1,12 +1,9 @@
 import { ISource } from '../database/schemas/source';
-export interface  ISourceProvider {
 
-
-    basepath() : string;
-    download(url : string, userId? : string) : Promise<ISource>;
-    sourceUrl(source : ISource) : string;
-    canHandle(url : string) : boolean;
-    sourceIdentifier() : string;
-
-
+export interface ISourceProvider {
+	basepath(): string;
+	download(url: string, userId?: string): Promise<ISource>;
+	sourceUrl(source: ISource): string;
+	canHandle(url: string): boolean;
+	sourceIdentifier(): string;
 }
