d9bebb5b23bcb383ea95e0b794b89b3ab329caa2
Sander <sanderbrokelman@msn.com>
2018-05-16 19:58:22 +0200

    Added a new UserService and error handling shizzle

--- src/database/config.ts
+++ src/database/config.ts
@@ -16,11 +16,11 @@ module.exports = function (passport: PassportStatic) {
 	passport.deserializeUser((id, done) => {
 		User.findById(id, (err, user) => {
 			done(err, user as any);
-        });
-    });
+		});
+	});
 
 	passport.use(new passportjwt.Strategy(opts, (jwt_payload, done) => {
-		User.findById(jwt_payload._id, (err, user) => {
+		User.findById(jwt_payload._id, { password: 0 }, (err, user) => { // exclude hashed password
 			if (err) { return done(err, false); }
 			if (user) {
 				return done(null, user);
